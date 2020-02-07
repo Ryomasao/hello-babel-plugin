3925a851113be01e7de5c85746c5078987dde311
Ryomasao <konoemario@gmail.com>
2020-02-07 17:03:52 +0900

    プラグインを別ファイルに切り出す

---
+++ plugins/replaceVarToConst.js
@@ -0,0 +1,25 @@
+//https://nodejs.org/api/modules.html#modules_the_module_object
+module.exports = babel => {
+  //https://babeljs.io/docs/en/babel-types
+  // isXXXでnodeのタイプを判断できる。
+  // visitorパターンでnodeのタイプごとの関数を書いてるから、関数内でもっかいtypeを判定する意図があんまりわからない
+  // → 処理単位が違うものがあるからだ
+  //   変数の場合、以下の構造になってる
+  //   VariableDeclaration
+  //    declarations: [ VariableDeclarator ]
+  //   このとき、 VariableDeclarator内の値もNode型(NumericLiteral)であるんだけど
+  //   visitorの再起処理の流れにはのってこない
+  //
+  const { types: t } = babel;
+  return {
+    visitor: {
+      VariableDeclaration: nodePath => {
+        if (nodePath.node.kind === "var") {
+          nodePath.node.kind = "const";
+        }
+      },
+      VariableDeclarator: nodePath => {},
+      BinaryExpression: nodePath => {}
+    }
+  };
+};
--- try-di.js
+++ try-di.js
@@ -1,31 +1,11 @@
 const { transform } = require("@babel/core");
 const { parseExpression } = require("@babel/parser");
+
+const replaceVarToConst = require("./plugins/replaceVarToConst");
+
 const src = "var a, b";
 
-const plugin = babel => {
-  //https://babeljs.io/docs/en/babel-types
-  // isXXXでnodeのタイプを判断できる。
-  // visitorパターンでnodeのタイプごとの関数を書いてるから、関数内でもっかいtypeを判定する意図があんまりわからない
-  // → 処理単位が違うものがあるからだ
-  //   変数の場合、以下の構造になってる
-  //   VariableDeclaration
-  //    declarations: [ VariableDeclarator ]
-  //   このとき、 VariableDeclarator内の値もNode型(NumericLiteral)であるんだけど
-  //   visitorの再起処理の流れにはのってこない
-  //
-  const { types: t } = babel;
-  return {
-    visitor: {
-      VariableDeclaration: nodePath => {
-        if (nodePath.node.kind === "var") {
-          nodePath.node.kind = "const";
-        }
-      },
-      VariableDeclarator: nodePath => {},
-      BinaryExpression: nodePath => {}
-    }
-  };
-};
+const plugin = replaceVarToConst;
 
 const { code } = transform(src, { plugins: [plugin] });
 // 変換後のコード
