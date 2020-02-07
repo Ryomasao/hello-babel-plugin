cad9bf2db3cd2c94f6bd5f90aea6a2509fb00381
Ryomasao <konoemario@gmail.com>
2020-02-07 15:43:33 +0900

    babelプラグインの簡単なサンプルを追加

--- try-babel-plugin.js
+++ try-babel-plugin.js
@@ -1,16 +1,31 @@
 const { transform } = require("@babel/core");
 
-const src = "1 +2";
+const src = "1+2";
 
-const plugin = obj => ({
-  visitor: {
-    BinaryExpression: nodePath => {
-      // traverserを自分で作ったときは、引数にはシンプルにnodeそのもをの渡してた
-      // babel本体では、nodePathっていうnodeをラップしているオブジェクトがあって
-      // nodeのparentだったり、scopeだったり便利な情報がいっぱいつまってる
-      console.log(nodePath);
+// pluginはオブジェクトを返す関数か、オブジェクトそのものを渡すことができる
+// 前者の方が、引数にbabelのbabel-traverseだったり、いろいろ便利なものが入っているのでおすすめとのこと
+// 以下は前者の関数パターン
+const plugin = babel => {
+  //const { traverse } = babel;
+
+  return {
+    // traverseする前に実行する処理
+    pre() {
+      console.log("pre", this);
+    },
+    // 以下の書き方でもいいと思ったけど、上記でthisの設定した値は、visitor関数の第2引数にstateとしてセットされる。
+    //pre: () => {
+    //  console.log("pre", this);
+    //},
+    visitor: {
+      BinaryExpression: nodePath => {
+        // traverserを自分で作ったときは、引数にはシンプルにnodeそのもをの渡してた
+        // babel本体では、nodePathっていうnodeをラップしているオブジェクトがあって
+        // nodeのparentだったり、scopeだったり便利な情報がいっぱいつまってる
+        console.log(nodePath.node.operator);
+      }
     }
-  }
-});
+  };
+};
 
 const { code } = transform(src, { plugins: [plugin] });
